c16cc03c2492880c491696695b487c97ab0342ce
unknown <you@example.com>
2021-08-31 20:09:41 +0900

    initial

---
+++ src/ManageProject.tsx
@@ -0,0 +1,30 @@
+import { Card, Container, Grid, Stack } from '@material-ui/core'
+
+const ManageProject = () => {
+	return (
+		<Grid container>
+			<Grid item xs={4}>
+				<Stack>
+					<h2>333</h2>
+					<h2>333</h2>
+					<h2>333</h2>
+					<h2>333</h2>
+					<Card>
+						<h2>333</h2>
+					</Card>
+					<Card>
+						<h2>333</h2>
+					</Card>
+				</Stack>
+			</Grid>
+			<Grid item xs={8}>
+				<Grid item>
+					<Card>
+						<h2>333</h2>
+					</Card>
+				</Grid>
+			</Grid>
+		</Grid>
+	)
+}
+export default ManageProject
